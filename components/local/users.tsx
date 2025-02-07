e59d339555938d23f8f71be610ce308b2387a21c
RUHAMYA Elie <elieruhamya20@gmail.com>
2025-02-07 10:28:39 +0200

    update users file*

--- components/local/users.tsx
+++ components/local/users.tsx
@@ -2,7 +2,6 @@ import ListUsers from "./list-users";
 import Search from "./search";
 
 export default function Users() {
-  const users = [1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6];
   return (
     <div
       className="row-span-full col-span-2 lg:col-span-1 rounded pr-3 pt-1 overflow-hidden hidden md:block"
