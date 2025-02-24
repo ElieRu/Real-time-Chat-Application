2476e13351d11ca5f79c8212f637839e6a849861
RUHAMYA Elie <elieruhamya20@gmail.com>
2025-02-24 16:49:10 +0200

    Chat with Specifique user

--- components/local/chat.tsx
+++ components/local/chat.tsx
@@ -17,7 +17,7 @@ export default function Chat({
     <div className="bg-gray-100 mt-1 col-span-4 md:col-span-2 rounded-lg p-4">
       {selected && <ChatBar user={user} onClick={onClick} />}
       {selected && <ChatContents selectedUser={user} />}
-      {selected && <ChatInput />}
+      {selected && <ChatInput selectedUser={user} />}
     </div>
   );
 }
